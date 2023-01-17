48551395f808149292b2a479b823eacb2d935b4e
Harshit Kumar <116452122+Harshitkumargit@users.noreply.github.com>
2023-01-17 10:27:27 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,13 @@
+m=int(input("Enter Marks : "))
+if m>80:
+    print("A")
+elif 60<=m<=80:
+    print("B")
+elif 50<=m<60:
+    print("C")
+elif 45<=m<50:
+    print("D")
+elif 25<=m<45:
+    print("E")
+elif m<25:
+    print("F")
